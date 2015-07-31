fde7867cbbf19b16d5cfe3d7ae20f6a10f7504af
Jamie Bird <j.bird1@lancaster.ac.uk>
2015-07-31 10:00:59 +0000

    stats uses pypcap included in pip for easier install

--- stats.py
+++ stats.py
@@ -1,47 +1,42 @@
-from pypcap import pcap
+import pcap
 import dpkt
 import socket
 import datetime
 
-x = pcap()
-x.pcap_create( 'eth1' )
-x.pcap_set_promisc( True )
-x.pcap_activate()
+proto = ['UDP', 'TCP']
 
+x = pcap.pcap(name='eth1',immediate=True, timeout_ms=10000)
 
 def ip_to_str(ip):
-    return socket.inet_ntop(socket.AF_INET, ip)
+	return socket.inet_ntop(socket.AF_INET, ip)
 
 # time TCP|UDP src_addr dst_addr src_port dst_port ip_len
-def pcap_callback(pkt, ts):
-    # do stuff with packet
-    eth = dpkt.ethernet.Ethernet(pkt)
+def pcap_callback(ts, pkt):
+	# do stuff with packet
+	eth = dpkt.ethernet.Ethernet(pkt)
 
-    if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-    	print 'Non IP Packet type not supported %s\n' % eth.data.__class__.__name__
-	return 
+	if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+    		print 'Non IP Packet type not supported %s\n' % eth.data.__class__.__name__
+		return 
 
-    ip = eth.data
-    proto = ip.data.__class__.__name__
+   	ip = eth.data
 
-    if proto != "UDP" or proto != "TCP":
-	print 'Non UDP or TCP datagram not supported ' + proto
-
+    	if ip.data.__class__.__name__ not in proto:
+		print 'Non UDP or TCP datagram not supported ' + ip.data.__class__.__name__
+		return
         
-    #print(str(datetime.datetime.utcfromtimestamp(ts))),
-    print(ts),
-    print(proto),
-    print(ip_to_str(ip.src)),
-    print(ip_to_str(ip.dst)),
+    	print(ts),
+    	print(ip.data.__class__.__name__),
+    	print(ip_to_str(ip.src)),
+    	print(ip_to_str(ip.dst)),
 
-    trans = ip.data
+    	trans = ip.data
 
-    print(trans.dport),
-    print(trans.sport),
-    print(ip.len),    
+    	print(trans.dport),
+    	print(trans.sport),
+    	print(ip.len),    
 
-    print " "
+    	print " "
 
-x.pcap_set_callback(pcap_callback)
-x.pcap_loop()
-
+while True:
+	x.loop(0, pcap_callback)
